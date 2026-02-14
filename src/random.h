4afa04862f7c46264154b5c038280958955eb0df
zuckerburger <131454746+zuckerburger@users.noreply.github.com>
2026-02-14 16:24:20 +1100

    Use global random generator

--- src/random.h
+++ src/random.h
@@ -1,13 +1,13 @@
 #include "vec3.h"
 #include <cstdint>
 #include <random>
+inline thread_local std::mt19937 generator(42);
 
 inline double random_double(double min, double max, uint32_t seed) {
   if (min > max) {
     std::clog << "Invalid use of random_vector: min must be <= max" << '\n';
     exit(0);
   }
-  std::mt19937 generator(seed);
   std::uniform_real_distribution<> dis{min, max};
   return dis(generator);
 }
@@ -17,7 +17,6 @@ inline Vec3 random_vector(double min, double max, uint32_t seed) {
     std::clog << "Invalid use of random_vector: min must be <= max" << '\n';
     exit(0);
   }
-  std::mt19937 generator(seed);
   std::uniform_real_distribution<> dis{min, max};
   while (true) {
     Vec3 rand_vec = Vec3(dis(generator), dis(generator), dis(generator));
